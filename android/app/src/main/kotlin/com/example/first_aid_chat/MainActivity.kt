aef2cdaefabdd4fc178f69330a7b77e630489d9c
A.R.Ishaq <abdulrahman7699@gmail.com>
2023-10-30 21:00:41 +0800

    1st Push

---
+++ android/app/src/main/kotlin/com/example/first_aid_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_aid_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:812614656044:web:ae0723ad9cfe40f0d9f189";
+  static String apiKey = "AIzaSyDGwJgfj_lGLCmn3yaPfOBbDjtiEEOUShY";
+  static String messagingSenderId = "812614656044";
+  static String projectId = "first-aid-chat";
+  final primaryColor = Color.fromARGB(255, 255, 47, 43);
+}
